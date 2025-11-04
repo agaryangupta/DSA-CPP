db3b3ae03ab4be86b4cd10b148483da95c26fbb8
agaryangupta <agaryangupta123@gmail.com>
2025-11-04 16:27:32 +0530

    done

--- code.cpp
+++ code.cpp
@@ -806,3 +806,26 @@ int main() {
     else
         cout << "false";
 }
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    set < int > s; 
+    for (int i = 0; i < n; i++)
+        s.insert(arr[i]);
+        
+    for(int x : s)
+        cout << x << " ";
+        
+    cout << endl << s.size();
+
+}
