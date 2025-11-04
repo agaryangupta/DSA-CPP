de99a386e7536691a00d8dd1423b6c6d09338403
agaryangupta <agaryangupta123@gmail.com>
2025-11-04 16:15:47 +0530

    done

--- code.cpp
+++ code.cpp
@@ -716,3 +716,93 @@ int main() {
     cout << count;
     return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPrime(int n){
+    if(n<=1)
+        return false;
+     for(int i = 2; i <= n/2; i++) {
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+}
+
+bool isEven(int n){
+    int sum=0;
+    while(n>0){
+        sum +=n%10;
+        n=n/10;
+    }
+    return (sum %2==0);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int count=0;
+    for (int i = a; i <= b; i++) {
+        if (isPrime(i) && isEven(i)) {
+            count++;
+        }
+    }
+    cout<<count;
+}
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    
+    int arr[a];
+       
+    for(int i = 0; i < a; i++)
+        cin >> arr[i];
+        
+    for(int i = 0; i < a; i++) {
+        int cnt = 0;
+        for(int j = i+1; j < a; j++) {
+            if(arr[j] < arr[i])
+                cnt++;
+        }
+         cout << cnt << " ";
+    }
+    return 0;
+}
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string rs(string s) {
+    string r = "";
+    for (int i = s.size() - 1; i >= 0; i--)
+        r += s[i];
+    return r;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    int n;
+    cin >> n;
+
+    int len = s.size();
+    n = n % len;
+
+    string left = s.substr(n) + s.substr(0, n);
+    string right = s.substr(len - n) + s.substr(0, len - n);
+
+    if (left == rs(left) || right == rs(right))
+        cout << "true";
+    else
+        cout << "false";
+}
